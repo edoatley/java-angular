728155c34880d7a2af911f052e3eee1a963b7d1d
edoatley <edoatley@gmail.com>
2021-07-26 17:15:51 +0100

    Remove lombok and try a run with paketo

--- src/main/java/com/edo/javaangular/model/User.java
+++ src/main/java/com/edo/javaangular/model/User.java
@@ -1,16 +1,13 @@
 package com.edo.javaangular.model;
 
-import lombok.*;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -26,5 +23,46 @@ public class User implements Serializable {
 
     public User(String name, String email) {
         this(0L, name, email);
+    }
+
+    User() {
+        this(0L, "", "");
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
     }
 }
--- src/main/java/com/edo/javaangular/rest/UserController.java
+++ src/main/java/com/edo/javaangular/rest/UserController.java
@@ -2,19 +2,20 @@ package com.edo.javaangular.rest;
 
 import com.edo.javaangular.model.User;
 import com.edo.javaangular.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @RestController
 @CrossOrigin(origins = "http://localhost:4200") // angular frontend
 public class UserController {
 
-    // standard constructors
-
     private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 
     @GetMapping("/users")
     public List<User> getUsers() {
